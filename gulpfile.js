ea3faeb29b0434a5d955fa1527a487c360b60a6d
Jérôme D.Soucy <jeromedsoucy@gmail.com>
2015-07-09 23:50:14 -0400

    Fix #20 - Add Sass support

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
   connect = require('gulp-connect'),
   open = require('gulp-open'),
   watch = require('gulp-watch'),
+  sass = require('gulp-sass'),
   babel = require("gulp-babel");
 var port = 8081;
 var src = './src';
@@ -27,15 +28,20 @@ gulp.task('js', function () {
     .pipe(connect.reload());
 });
 
-gulp.task('watch', function () {
-  gulp.watch([src + '/js/*.js'], ['js', 'babel']);
+gulp.task('sass', function () {
+  gulp.src(src + '/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(dist + '/css'));
 });
 
 gulp.task("babel", function () {
   return gulp.src("./js/*.js")
     .pipe(babel())
-    .pipe(gulp.dest(dist + "/js"));
 });
 
+gulp.task('watch', function () {
+  gulp.watch([src + '/js/*.js'], ['js', 'babel']);
+  gulp.watch(src + '/sass/*.scss', ['sass']);
+});
 
-gulp.task('default', ['babel', 'connect', 'open', 'watch']);
+gulp.task('default', ['sass', 'babel', 'connect', 'open', 'watch']);
